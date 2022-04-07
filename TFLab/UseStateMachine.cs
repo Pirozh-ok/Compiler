45e6e8c63ae2c2986a0744c4f662e1a0d521e815
Ivan Vorotnikov <ivan.vorotnikov.2002@mail.ru>
2022-04-07 10:18:09 +0700

    Внёс изменения по 4 лаб работе

--- TFLab/UseStateMachine.cs
+++ TFLab/UseStateMachine.cs
@@ -12,32 +12,30 @@ namespace TFLab
         static List<StateMachine> _listState = new List<StateMachine>()
         {
             new StateMachine(0, false, new List<Transition>{new Transition(1, 'a')}),
-            new StateMachine(1, false, new List<Transition>{new Transition(2, 'b')}),
-            new StateMachine(2, false, new List<Transition>{new Transition(3, 'c'), new Transition(4, 'd')}),
-            new StateMachine(3, false, new List<Transition>{new Transition(5, 'e'), new Transition(6, 'f')}),
-            new StateMachine(4, false, new List<Transition>{new Transition(1, 'k'), new Transition (7, 'g')}),
-            new StateMachine(5, false, new List<Transition>{new Transition(8, 'n'), new Transition(1, 'i')}),
-            new StateMachine(6, false, new List<Transition>{new Transition(8, 'n'), new Transition(1, 'i')}),
-            new StateMachine(7, false, new List<Transition>{new Transition(8, 'n')}),
-            new StateMachine(8, false, new List<Transition>{new Transition(9, 'm')}),
-            new StateMachine(9, false, new List<Transition>{new Transition(10, 'l')}),
-            new StateMachine(10, true, new List<Transition>{})
+            new StateMachine(1, false, new List<Transition>{new Transition(2, 'c'), new Transition(3, 'd')}),
+            new StateMachine(2, false, new List<Transition>{new Transition(4, 'e'), new Transition(5, 'f')}),
+            new StateMachine(3, false, new List<Transition>{new Transition(1, 'k'), new Transition (7, 'n')}),
+            new StateMachine(4, false, new List<Transition>{new Transition(1, 'i'), new Transition(6, 'g')}),
+            new StateMachine(5, false, new List<Transition>{new Transition(1, 'i'), new Transition(6, 'g')}),
+            new StateMachine(6, false, new List<Transition>{new Transition(7, 'n')}),
+            new StateMachine(7, false, new List<Transition>{new Transition(8, 'm')}),
+            new StateMachine(8, false, new List<Transition>{new Transition(9, 'l')}),
+            new StateMachine(9, true, new List<Transition>{})
         };
 
         static Dictionary<char, string> _descriptionState = new Dictionary<char, string>()
         {
             {'a', "переход в состояние 1 - поставить бутыку" },
-            {'b', "переход в состояние 2 - выбрать способ оплаты" },
-            {'c', "переход в состояние 3 - выбран способ оплаты наличными" },
-            {'d', "переход в состояние 4 - выбран способ оплаты по карте" },
-            {'e', "переход в состояние 5 - оплата монетами" },
-            {'f', "переход в состояние 6 - оплата купюрами" },
-            {'i', "переход в состояние 1 - купюра/монеты автомат не принял" },
-            {'k', "переход в состояние 1 - сервер не ответил" },
-            {'g', "переход в состояние 7 - сервер ответил" },
-            {'n', "переход в состояние 8 - оплата прошла успешно" },
-            {'m', "переход в состояние 9 - налить молоко" },
-            {'l', "переход в состояние 10 - забрать молоко" }
+            {'c', "переход в состояние 2 - выбран способ оплаты наличными" },
+            {'d', "переход в состояние 3 - выбран способ оплаты по карте" },
+            {'e', "переход в состояние 4 - оплата монетами" },
+            {'f', "переход в состояние 5 - оплата купюрами" },
+            {'i', "переход в состояние 1 - купюру/монеты автомат не принял" },
+            {'k', "переход в состояние 1 - оплата по карте не прошла" },
+            {'g', "переход в состояние 6 - выдача сдачи" },
+            {'n', "переход в состояние 7 - оплата прошла успешно" },
+            {'m', "переход в состояние 8 - налить молоко" },
+            {'l', "переход в состояние 9 - забрать молоко" }
         };
         static public string StartAnalize(string[] strings)
         {
