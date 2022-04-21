c370b6e540e73ffb964e888eca1e218a951491da
Ivan Vorotnikov <ivan.vorotnikov.2002@mail.ru>
2022-04-21 09:35:42 +0700

    начал делать рекурсивный спуск

---
+++ TFLab/IRecursiveFunction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFLab
+{
+    internal interface IRecursiveFunction
+    {
+        // Выражение
+        void Expression();
+        // Слагаемое
+        void Term();
+        // Множитель
+        void Factor();
+        // Дробное число
+        void FractionalNumber();
+        // Функция
+        void Function();
+        // Целая часть
+        void WholePart();
+        // Дробная часть
+        void Fraction();
+        // Цифра
+        void Digit();
+        // Имя функции
+        void NameFunction();
+    }
+}
---
+++ TFLab/RecurciveAnalyzer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFLab
+{
+    internal class RecurciveAnalyzer : IRecursiveFunction
+    {
+        private string _text; 
+        private 
+        RecurciveAnalyzer(string text)
+        {
+            _text = text;
+            _text.Replace("\n", "");
+            _text.Replace(" ", "");
+        }
+
+        public void Expression()
+        {
+            Term();
+        }
+
+        public void Term()
+        {
+            throw new NotImplementedException();
+        }
+        public void Digit()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public void Factor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fraction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FractionalNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Function()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NameFunction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WholePart()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
