8bf6a70211167c0358fcc0b2c44bf5e306dee4ff
Ivan Vorotnikov <ivan.vorotnikov.2002@mail.ru>
2022-04-21 15:29:19 +0700

    добавил дополнение парсером недостоющих лексем для нейтрализации ошибок

--- TFLab/UseStateMachine.cs
+++ TFLab/UseStateMachine.cs
@@ -15,7 +15,7 @@ namespace TFLab
             new StateMachine('B', false, new List<Transition>{new Transition('C', '/')}),
             new StateMachine('C', false, new List<Transition>{new Transition('C', 's'), new Transition('K', '\n')}),
             new StateMachine('D', false, new List<Transition>{new Transition('F', '*')}),
-            new StateMachine('F', false, new List<Transition>{new Transition('F', 's'), new Transition('G', '*'), new Transition('F', '\n')}),
+            new StateMachine('F', false, new List<Transition>{new Transition('G', '*'),new Transition('F', 's'), new Transition('F', '\n')}),
             new StateMachine('G', false, new List<Transition>{new Transition('K',')')}),
             new StateMachine('K', true, null)
         };
@@ -24,7 +24,6 @@ namespace TFLab
         {
             const char startState = 'I';
             char currentState = startState;
-            char previousState = startState; 
             string result = string.Empty;
             bool isMultiStringComment = false;
             int i = 0;
@@ -36,19 +35,17 @@ namespace TFLab
                 //определяем следующее состояние 
                 var nextState = _listState.FirstOrDefault(x => x.State == currentState).Transitions.FirstOrDefault(x => x.transition == transition);
 
-                //если нет перехода по считанному символу, значит неверная последовательность
+                //если нет перехода по считанному символу, значит неверная последовательность или пропущена лексема
                 if (nextState == null)
                 {
-                    var errorSymbol = lexems[i].Item1 == '\n' ? "\\n" : lexems[i].Item1.ToString();
-                    errors.Add($"Строка {lexems[i].Item2}: имя \"{errorSymbol}\" не существует в текущем контексте");
-                    //возвращаемся в состояние до ошибки и продолжаем разбор
-                    currentState = previousState;
+                    char expectedSymbol = _listState.FirstOrDefault(x => x.State == currentState).Transitions[0].transition;
+                    errors.Add($"Строка {lexems[i].Item2}: ожидается \"{expectedSymbol}\", подаётся \"{lexems[i].Item1}\"");
+                    lexems.Insert(i, (expectedSymbol, lexems[i].Item2));
+                    i--;
                 }
                 //если такой переход есть, переходим в след состояние
                 else
                 {
-                    //запоминаем предыдущее состояние, для нейтрализации в случае ошибки
-                    previousState = currentState;
                     currentState = nextState.state;
 
                     //если многострочный комментарий, то устанавливаем флаг
@@ -69,4 +66,4 @@ namespace TFLab
             return errors;
         }
     }
-}
+} 
